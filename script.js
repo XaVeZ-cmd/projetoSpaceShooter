74debe4d1cb0ab6e4801f834be38459eaf03ed3e
XaVeZcmd <thiago.amorim@zipmail.com.br>
2020-11-09 13:14:40 +0000

    Implementado inimigos

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const yourShip = document.querySelector('.player-shooter');
 const playArea = document.querySelector('#main-play-area');
+const aliensImg = ['img/monster-1.png', 'img/monster-2.png', 'img/monster-3.png'];
 
 
 //movimento e tiro da nave
@@ -69,6 +70,18 @@ function moveLaser(laser){
       laser.style.left = `${xPosition + 8}px`;
     }
   }, 10);
+}
+//função para criar inimigoa aleatórios
+function createAliens(){
+  let newAlien = document.createElement('img');
+  let alienSprite = aliensImg[Math.floor(Math.random()*aliensImg.length)]; //sorteio de imagens
+  newAlien.src = alienSprite;
+  newAlien.classList.add('alien');
+  newAlien.classList.add('alien-transition');
+  newAlien.style.left = '370px';
+  newAlien.style.top = `${Math.floor(Math.random()*330)+30}px`;
+  playArea.appendChild(newAlien);
+  moveAlien(newAlien);
 }
 
 window.addEventListener('keydown', flyShip);
