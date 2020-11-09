a1fa1bd19975274bc76cc2e482c0e3cf40204634
XaVeZcmd <thiago.amorim@zipmail.com.br>
2020-11-09 12:50:43 +0000

    Funcionalidade de tiro

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const yourShip = document.querySelector('player-shooter');
-const playArea = document.querySelector('main-play-game');
+const yourShip = document.querySelector('.player-shooter');
+const playArea = document.querySelector('#main-play-game');
 
 
 //movimento e tiro da nave
 function flyShip(event){
   if(event.key === 'ArrowUp'){
     event.preventDefault();
-    meveUp();
+    moveUp();
   }else if(event.key === 'ArrowDown'){
     event.preventDefault();
     moveDown();
@@ -24,18 +24,43 @@ function moveUp(){
   }else{
     let position = parseInt(topPosition);
     position -= 50;
-    yourShip.style.top = `$(position)px`;
+    yourShip.style.top = `${position}px`;
   }
 }
 
 //função de descer
 function moveDown(){
   let topPosition = getComputedStyle(yourShip).getPropertyValue('top');
-  if(topPosition === "540px"){
+  if(topPosition === "500px"){
     return
   }else{
     let position = parseInt(topPosition);
     position += 50;
-    yourShip.style.top = `$(position)px`;
+    yourShip.style.top = `${position}px`;
   }
 }
+
+//funcionalidade de tiro
+
+function fireLaser(){
+  let laser = createLaserElement();
+  playArea.appendChild(laser);
+  moveLaser();
+}
+
+function createLaserElement(){
+  let xPosition = parseInt(window.getComputedStyle(yourShip).getPropertyValue('left'));
+  let yPosition = parseInt(window.getComputedStyle(yourShip).getPropertyValue('top'));
+  let newLaser = document.createElement('img');
+  newLaser.src = 'img/shoot.png';
+  newLaser.classList.add('laser');
+  newLaser.style.left = `${xPosition}px`;
+  newLaser.style.top = `${yPosition - 10}px`;
+  return newLaser;
+}
+
+function moveLaser(){
+  
+}
+
+window.addEventListener('keydown', flyShip);
